203dd5524a04e64037f94925af41aa2a27701060
Imtbdev <imtbdev@gmail.com>
2025-04-18 19:55:37 +0500

    Auth added

--- djangosite/blog/urls.py
+++ djangosite/blog/urls.py
@@ -1,7 +1,11 @@
-from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path("", views.PostList.as_view(), name="home"),
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name="logout"),
+    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
 ]
