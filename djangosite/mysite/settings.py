c2f9559b7664f0e126b7d9bda71b12dd9d399328
Imtbdev <imtbdev@gmail.com>
2025-04-18 19:56:55 +0500

    Auth fix

--- djangosite/mysite/settings.py
+++ djangosite/mysite/settings.py
@@ -109,14 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "test4allservices@gmail.com"
-EMAIL_HOST_PASSWORD = "s4!2NEMgj5cZq!."
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
